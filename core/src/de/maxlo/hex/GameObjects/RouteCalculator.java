1e0e64fe3076a9bea80ec1c2af51e642d6f15b9c
Max-Lo <max_lorenz@gmx.de>
2017-10-29 20:35:59 +0100

    added a*

--- core/src/de/maxlo/hex/GameObjects/RouteCalculator.java
+++ core/src/de/maxlo/hex/GameObjects/RouteCalculator.java
@@ -1,132 +1,177 @@
 package de.maxlo.hex.GameObjects;
 
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ObjectMap;
 
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
- * Created by moritz on 02.10.17.
+ * Created by max on 28.10.17.
  */
 
-public final class RouteCalculator{
-    private static final int AMMOUNT_OF_TMP_SETS = 40; // should euquals the ammount of HexFields
+public class RouteCalculator {
 
-    private Set<Vector3> nodeSet;
-    private Map<Vector3, Set<Vector3>> verticesMap;
+    /**
+     * calculate a route from start to destination using the A* algorithm
+     *
+     * returns null if no route exists otherwise the route as a list with Vector3
+     */
+    public List<Vector3> calculateRoute(ObjectMap<Vector3, Hexagon> hexagons, Vector3 start, Vector3 destination) {
+        if (!hexagons.containsKey(start))
+            throw new IllegalArgumentException("Map doesn't contain start field");
+        else if (!hexagons.containsKey(destination))
+            throw new IllegalArgumentException("Map doesn't contain destination field");
 
-    private List<Set<Vector3>> tmpSetList;
-
-    private List<Vector3> frontier;
-    private List<Vector3> tmpFrontier;
-    private List<Vector3> waypoints;
-    private List<Vector3> closedNodes;
-    private int distance_from_source;
-    private Comparator<Vector3> priorityComperator;
+        // TODO remove after finishing
+        System.out.println("Start: " + start);
+        System.out.println("Destination: " + destination);
 
 
-    private RouteCalculator(){//TODO private construct was a hint on stackoverflow (static class java)
-        initialise();
+        // convert the ObjectMap<Vector3, Hexagon> because the Hexagon object isn't of interest
+        // and the cost and predecessor has to be stored someway
+        ObjectMap<Vector3, PredecessorCostTuple> undiscoveredMap = new ObjectMap<Vector3, PredecessorCostTuple>();
+        for (ObjectMap.Entry<Vector3, Hexagon> entry : hexagons ) {
+            // init cost with a number where it's sure it is never reached
+            undiscoveredMap.put(entry.key, null);
+        }
+        undiscoveredMap.remove(start);
+
+        ObjectMap<Vector3, PredecessorCostTuple> discoveredMap = new ObjectMap<Vector3, PredecessorCostTuple>();
+
+        ObjectMap<Vector3, PredecessorCostTuple> nextCandidates = new ObjectMap<Vector3, PredecessorCostTuple>();
+        nextCandidates.put(start, new PredecessorCostTuple(null, getDistance(start, destination)));
+
+        boolean destinationFound = false;
+
+        while (nextCandidates.size > 0 && !destinationFound) {
+
+            Vector3 currentField = getFieldWithLowestCost(nextCandidates);
+            int old_cost = nextCandidates.get(currentField).cost;
+
+            for (Vector3 pos : getNeighbours(currentField)) {
+
+                // check whether destination is found
+                if (pos.equals(destination)) {
+                    destinationFound = true;
+                    discoveredMap.put(pos, new PredecessorCostTuple(currentField, getDistance(start, destination)));
+                }
+                // only calculate cost if field exists
+                else if (undiscoveredMap.containsKey(pos)) {
+                    // add field to candidates
+                    Integer cost = old_cost+1 + getDistance(pos, destination);
+                    nextCandidates.put(pos, new PredecessorCostTuple(currentField, cost));
+                    undiscoveredMap.remove(pos);
+                }
+            }
+
+            discoveredMap.put(currentField, nextCandidates.get(currentField));
+            nextCandidates.remove(currentField);
+        }
+
+
+        if (!destinationFound)
+            return null;
+        else {
+            // TODO remove after finishing
+            List<Vector3> route = reconstructRoute(discoveredMap, destination);
+            System.out.println("Route:");
+            System.out.println(route);
+            return route;
+        }
     }
 
-    public List<Vector3> calculateRoute(GameMap map, Player player, Vector3 start, Vector3 destination){
-        clearAll();
-        constructNodeSet(map, player);
-        constructVerticesMap();
-        return aStar(start, destination);
+    /**
+     * takes the given destination field and tracks it back to the start point while constructing the route
+     */
+    private static List<Vector3> reconstructRoute(ObjectMap<Vector3, PredecessorCostTuple> map, Vector3 destination) {
+        if (map == null || destination == null)
+            throw new NullPointerException();
+        if (!map.containsKey(destination))
+            throw new IllegalArgumentException("Map must contain destination!" + destination);
+
+        List<Vector3> route = new ArrayList<Vector3>();
+
+        Vector3 currentField = destination;
+        while (map.get(currentField).predecessor != null) {
+            route.add(currentField);
+            currentField = map.get(currentField).predecessor;
+        }
+        route.add(currentField);
+
+        return route;
     }
 
-    private List<Vector3> aStar(Vector3 start, Vector3 destination) {
-        //trick 17: z coord of Vector3 is priority
-        distance_from_source = 0;
-        start.z = 0;
-        frontier.add(start);
-        while (!frontier.isEmpty()){
-            frontier.sort(priorityComperator);
-            distance_from_source++;              //TODO
-            for (Vector3 currentNode : frontier){
-                if (currentNode == destination){
-                    closedNodes.add(currentNode);
-                    break;
-                }        //maybe add it as last point to waypoints
 
-                tmpFrontier.addAll(verticesMap.get(currentNode));
-                for (Vector3 currentAdjancentNode : tmpFrontier)
-                    //priority is distance from goal + distance from source
-                    currentAdjancentNode.z = Math.abs(destination.x - currentAdjancentNode.x) + Math.abs(destination.y - currentAdjancentNode.y) + distance_from_source;
+    /**
+     * get the shortest possible way between two hexagon fields (assuming there are no holes or
+     * obstacles
+     */
+    private static int getDistance(Vector3 pos1, Vector3 pos2) {
+        return (int)(Math.abs(pos1.x-pos2.x) + Math.abs(pos1.y-pos2.y));
+    }
 
-                closedNodes.add(currentNode);
-                frontier.remove(currentNode);
+    private static Vector3 getFieldWithLowestCost(ObjectMap<Vector3, PredecessorCostTuple> map) {
+        if (map == null)
+            throw new NullPointerException();
 
+        ObjectMap.Entry<Vector3, PredecessorCostTuple> bestField = null;
+        for (ObjectMap.Entry<Vector3, PredecessorCostTuple> entry : map) {
+            if (bestField == null)
+                bestField = entry;
+            else if (bestField.value.cost > entry.value.cost)
+                bestField = entry;
+        }
+
+        assert bestField != null;
+        return bestField.key;
+    }
+
+    /**
+     * Helper class to store predecessor and cost in one map entry
+     */
+    private class PredecessorCostTuple {
+
+        private Vector3 predecessor;
+        private Integer cost;
+
+        public PredecessorCostTuple(Vector3 predecessor, Integer cost) {
+            this.predecessor = predecessor;
+            this.cost = cost;
+        }
+    }
+
+    /**
+     * Constructs a map with only fields that are owned by the given owner
+     * @param map - original map where none owner field should be removed from
+     * @param owner - owner of the desired fields
+     * @return - map only with fields being owned by the owner
+     */
+    public static ObjectMap<Vector3, Hexagon> constructOwnerMap(ObjectMap<Vector3, Hexagon> map, Player owner) {
+        ObjectMap<Vector3, Hexagon> ownerMap = new ObjectMap<Vector3, Hexagon>();
+
+        for (ObjectMap.Entry<Vector3, Hexagon> entry : map) {
+            if (entry.value.getOwner().equals(owner)) {
+                ownerMap.put(entry.key, entry.value);
             }
         }
-        return closedNodes;
+        return ownerMap;
     }
 
-    private void constructVerticesMap() {
-        int countOfTmpSets = 0;
-        for (Vector3 currentNode : nodeSet){
-            verticesMap.put(currentNode, tmpSetList.get(countOfTmpSets));
-            countOfTmpSets++;
-            for (Vector3 adjacentNode : nodeSet)
-                //complicated if explanation:
-                //1st big expression: checking directly above and below ( x++ / x-- while y const)
-                //2nd big expression: checking diagonal lower right and upper left ( x const while y++ / y-- )
-                //3rd big expression: checking diagonal lower left and upper right (x++ && y++ || x-- && y-- )
-                //4th big expression: check not to make currentCoords also as adjancent (xc != xa && yc != ya)
-                if ((((currentNode.x == adjacentNode.x - 1) || currentNode.x == adjacentNode.x + 1) && currentNode.y == adjacentNode.y) || (currentNode.x == adjacentNode.x && ((currentNode.y == adjacentNode.y +1) || (currentNode.y == adjacentNode.y-1) )) || (currentNode.x == adjacentNode.x - 1 && currentNode.y == adjacentNode.y -1) || (currentNode.x == adjacentNode.x + 1 && currentNode.y == adjacentNode.y +1) && (currentNode.x != adjacentNode.x && currentNode.y != adjacentNode.y))
-                    verticesMap.get(currentNode).add(adjacentNode);
-        }
+    /**
+     * returns a list with Vector3's containing the surrounding fields positions
+     */
+    private static List<Vector3> getNeighbours(Vector3 pos) {
+        List<Vector3> neighbours = new ArrayList<Vector3>();
+
+        neighbours.add(new Vector3(pos.x, pos.y+1, 0));
+        neighbours.add(new Vector3(pos.x+1, pos.y+1, 0));
+        neighbours.add(new Vector3(pos.x+1, pos.y, 0));
+        neighbours.add(new Vector3(pos.x, pos.y-1, 0));
+        neighbours.add(new Vector3(pos.x-1, pos.y-1, 0));
+        neighbours.add(new Vector3(pos.x-1, pos.y, 0));
+
+        return neighbours;
     }
 
-    private void initialise() {
-        nodeSet = new HashSet<Vector3>();
-        verticesMap = new HashMap<Vector3, Set<Vector3>>();
-
-        frontier = new ArrayList<Vector3>();
-        waypoints = new ArrayList<Vector3>();
-
-        tmpSetList = new ArrayList<Set<Vector3>>();
-        for (int i = 0; i<AMMOUNT_OF_TMP_SETS; i++)
-            tmpSetList.add(new HashSet<Vector3>());
-
-        priorityComperator = new Comparator<Vector3>() {
-            @Override
-            public int compare(Vector3 vector3, Vector3 t1) {
-                if (vector3.z < t1.z)
-                    return -1;
-                else if (vector3.z > t1.z)
-                    return 1;
-                else
-                    return 0;
-            }
-        };
-    }
-
-    private void clearAll() {
-        waypoints.clear();
-        //clear frontier
-        frontier.clear();
-        //clear nodesSet
-        nodeSet.clear();
-        //clear verticesMap
-        verticesMap.clear();
-        //clear tmpSets
-        for (Set<Vector3> tmpSet : tmpSetList)
-            tmpSet.clear();
-        distance_from_source = 0;
-        heuristic = 0;
-    }
-
-    private void constructNodeSet(GameMap map, Player player){
-        //checks each hexagon if the current owner is the given player
-        for (Vector3 coordinates : map.getHexagons().keySet())
-            if (player == map.getHexagon(coordinates).getOwner())
-                nodeSet.add(coordinates);
-    }
 }
--- core/src/de/maxlo/hex/GameObjects/Travel.java
+++ core/src/de/maxlo/hex/GameObjects/Travel.java
@@ -4,12 +4,7 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ObjectMap;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-
-import de.maxlo.hex.Hex;
-import javafx.collections.transformation.SortedList;
 
 /**
  * Created by max on 27.09.17.
@@ -22,6 +17,7 @@ public class Travel {
     private Vector3 destination;
     private Player player;
     private int units;
+    private RouteCalculator routeCalculator;
 
     private float progress; // float between 0 and 100
     private List<Vector3> wayPoints;
@@ -30,12 +26,14 @@ public class Travel {
         this.start = start;
         this.destination = destination;
         progress = 0.0f;
+
+        routeCalculator = new RouteCalculator();
     }
 
     public boolean start(ObjectMap<Vector3, Hexagon> hexagons, Player player) {
-        ObjectMap<Vector3, Hexagon> ownerMap = constructOwnerMap(hexagons, player);
+        ObjectMap<Vector3, Hexagon> ownerMap = routeCalculator.constructOwnerMap(hexagons, player);
 
-        wayPoints = calculateRoute(ownerMap, start, destination);
+        wayPoints = routeCalculator.calculateRoute(ownerMap, start, destination);
 
         return wayPoints != null;
     }
@@ -56,191 +54,4 @@ public class Travel {
             return false;
     }
 
-    /**
-     * returns null if no route exists
-     *
-     * --- A* algorithm ---
-     * evaluated full path costs
-     *
-     * full_cost - f(n),
-     * cost_to_current_point - c(n)
-     * cost_to_destination - d(n)
-     *
-     * Steps:
-     * 1) calculate full_cost for every unexplored neighbour
-     * 2) choose field with the lowest cost and expand it
-     * 3) if destination is not reached yet calculate costs for the new explored field
-     */
-    private List<Vector3> calculateRoute(ObjectMap<Vector3, Hexagon> hexagons, Vector3 start, Vector3 destination) {
-        if (!hexagons.containsKey(start))
-            throw new IllegalArgumentException("Map doesn't contain start field");
-        else if (!hexagons.containsKey(destination))
-            throw new IllegalArgumentException("Map doesn't contain destination field");
-
-        System.out.println("Start: " + start);
-        System.out.println("Destination: " + destination);
-
-
-        // convert the ObjectMap<Vector3, Hexagon> because the Hexagon object isn't of interest
-        // and the cost and predecessor has to be stored someway
-        ObjectMap<Vector3, PredecessorCostTuple> undiscoveredMap = new ObjectMap<Vector3, PredecessorCostTuple>();
-        for (ObjectMap.Entry<Vector3, Hexagon> entry : hexagons ) {
-            // init cost with a number where it's sure it is never reached
-            undiscoveredMap.put(entry.key, null);
-        }
-        undiscoveredMap.remove(start);
-
-        ObjectMap<Vector3, PredecessorCostTuple> discoveredMap = new ObjectMap<Vector3, PredecessorCostTuple>();
-
-        ObjectMap<Vector3, PredecessorCostTuple> nextCandidates = new ObjectMap<Vector3, PredecessorCostTuple>();
-        nextCandidates.put(start, new PredecessorCostTuple(null, getDistance(start, destination)));
-
-        boolean destinationFound = false;
-
-        while (nextCandidates.size > 0 && !destinationFound) {
-
-            Vector3 currentField = getFieldWithLowestCost(nextCandidates);
-            int old_cost = nextCandidates.get(currentField).cost;
-
-            for (Vector3 pos : getNeighbours(currentField)) {
-
-                // check whether destination is found
-                if (pos.equals(destination)) {
-                    destinationFound = true;
-                    discoveredMap.put(pos, new PredecessorCostTuple(currentField, getDistance(start, destination)));
-                }
-                // only calculate cost if field exists
-                else if (undiscoveredMap.containsKey(pos)) {
-                    // add field to candidates
-                    Integer cost = old_cost+1 + getDistance(pos, destination);
-                    nextCandidates.put(pos, new PredecessorCostTuple(currentField, cost));
-                    undiscoveredMap.remove(pos);
-                }
-            }
-
-            discoveredMap.put(currentField, nextCandidates.get(currentField));
-            nextCandidates.remove(currentField);
-        }
-
-
-        if (!destinationFound)
-            return null;
-        else {
-            // TODO remove after finishing
-            List<Vector3> route = reconstructRoute(discoveredMap, destination);
-            System.out.println("Route:");
-            System.out.println(route);
-            return route;
-        }
-    }
-
-    /**
-     * takes the given destination field and tracks it back to the start point while constructing the route
-     */
-    private List<Vector3> reconstructRoute(ObjectMap<Vector3, PredecessorCostTuple> map, Vector3 destination) {
-        if (map == null || destination == null)
-            throw new NullPointerException();
-        if (!map.containsKey(destination))
-            throw new IllegalArgumentException("Map must contain destination!" + destination);
-
-        List<Vector3> route = new ArrayList<Vector3>();
-
-        Vector3 currentField = destination;
-        while (map.get(currentField).getPredecessor() != null) {
-            route.add(currentField);
-            currentField = map.get(currentField).getPredecessor();
-        }
-        route.add(currentField);
-
-        return route;
-    }
-
-
-    /**
-     * get the shortest possible way between two hexagon fields (assuming there are no holes or
-     * obstacles
-     */
-    private int getDistance(Vector3 pos1, Vector3 pos2) {
-        return (int)(Math.abs(pos1.x-pos2.x) + Math.abs(pos1.y-pos2.y));
-    }
-
-    private Vector3 getFieldWithLowestCost(ObjectMap<Vector3, PredecessorCostTuple> map) {
-        if (map == null)
-            throw new NullPointerException();
-
-        ObjectMap.Entry<Vector3, PredecessorCostTuple> bestField = null;
-        for (ObjectMap.Entry<Vector3, PredecessorCostTuple> entry : map) {
-            if (bestField == null)
-                bestField = entry;
-            else if (bestField.value.cost > entry.value.cost)
-                bestField = entry;
-        }
-
-        assert bestField != null;
-        return bestField.key;
-    }
-
-    /**
-     * Helper class to store predecessor and cost in one map entry
-     */
-    private class PredecessorCostTuple {
-
-        private Vector3 predecessor;
-        private Integer cost;
-
-        public PredecessorCostTuple(Vector3 predecessor, Integer cost) {
-            this.predecessor = predecessor;
-            this.cost = cost;
-        }
-
-        public Vector3 getPredecessor() {
-            return predecessor;
-        }
-
-        public void setPredecessor(Vector3 predecessor) {
-            this.predecessor = predecessor;
-        }
-
-        public Integer getCost() {
-            return cost;
-        }
-
-        public void setCost(Integer cost) {
-            this.cost = cost;
-        }
-    }
-
-    /**
-     * Constructs a map with only fields that are owned by the given owner
-     * @param map - original map where none owner field should be removed from
-     * @param owner - owner of the desired fields
-     * @return - map only with fields being owned by the owner
-     */
-    private ObjectMap<Vector3, Hexagon> constructOwnerMap(ObjectMap<Vector3, Hexagon> map, Player owner) {
-        ObjectMap<Vector3, Hexagon> ownerMap = new ObjectMap<Vector3, Hexagon>();
-
-        for (ObjectMap.Entry<Vector3, Hexagon> entry : map) {
-            if (entry.value.getOwner().equals(owner)) {
-                ownerMap.put(entry.key, entry.value);
-            }
-        }
-        return ownerMap;
-    }
-
-    /**
-     * @param pos their neighbours should be returned
-     * @return neighbours of pos
-     */
-    private List<Vector3> getNeighbours(Vector3 pos) {
-        List<Vector3> neighbours = new ArrayList<Vector3>();
-
-        neighbours.add(new Vector3(pos.x, pos.y+1, 0));
-        neighbours.add(new Vector3(pos.x+1, pos.y+1, 0));
-        neighbours.add(new Vector3(pos.x+1, pos.y, 0));
-        neighbours.add(new Vector3(pos.x, pos.y-1, 0));
-        neighbours.add(new Vector3(pos.x-1, pos.y-1, 0));
-        neighbours.add(new Vector3(pos.x-1, pos.y, 0));
-
-        return neighbours;
-    }
 }
